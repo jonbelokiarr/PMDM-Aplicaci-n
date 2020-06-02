f88fdca01410081b2ae976b899347d21819b5f2e
jonbelokiarr <jonbeloki52@gmail.com>
2020-06-03 01:58:12 +0200

    He decidido cambiar la estructura del proyecto. En este commit va la nueva forma del proyecto con los comandos de la cámara introducidos y la navegacion entre los fragments hecho. Solo que me da un pequeño error.

--- PMDM_Aplicacion/app/src/main/java/com/example/pmdm_aplicacion/MainActivity.java
+++ PMDM_Aplicacion/app/src/main/java/com/example/pmdm_aplicacion/MainActivity.java
@@ -2,38 +2,43 @@ package com.example.pmdm_aplicacion;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
 
+import com.example.pmdm_aplicacion.fragments.CamaraFragment;
+import com.example.pmdm_aplicacion.fragments.CompartirFragment;
+import com.example.pmdm_aplicacion.fragments.GaleriaFragment;
 import com.frosquivel.magicalcamera.MagicalCamera;
 import com.frosquivel.magicalcamera.MagicalPermissions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.navigation.NavigationView;
 
-import androidx.viewpager.widget.ViewPager;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import com.example.pmdm_aplicacion.ui.main.SectionsPagerAdapter;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private MagicalPermissions magicalPermissions;
-    private final static int pixeles = 50;
+    private AppBarConfiguration mAppBarConfiguration;
+    private MagicalPermissions magicalPermisos;
+    private final static int tamaño = 50;
     private MagicalCamera magicalCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
-
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,16 +46,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setAction("Action", null).show();
             }
         });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment); /*ME FALLA AQUI*/
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
-        findViewById(R.id.button3).setOnClickListener(this);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.contenedor, new CamaraFragment())
+                .commit();
+
+        findViewById(R.id.button2).setOnClickListener(this);
 
         String[] permisos = new String[] {
                 Manifest.permission.CAMERA,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
         };
-        magicalPermissions = new MagicalPermissions(this, permisos);
-        magicalCamera = new MagicalCamera(this, pixeles, magicalPermissions);
+        magicalPermisos = new MagicalPermissions(this, permisos);
+
+        magicalCamera = new MagicalCamera(this, tamaño, magicalPermisos);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment); /*ME FALLA AQUI*/
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Fragment f = null;
+
+        if (id == R.id.nav_home) {
+            f = new CamaraFragment();
+        }
+        else if (id == R.id.nav_gallery) {
+            f = new GaleriaFragment();
+        }
+        else if (id == R.id.nav_slideshow) {
+            f = new CompartirFragment();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.contenedor, f)
+                .commit();
+
+        return true;
     }
 
     @Override
---
+++ PMDM_Aplicacion/app/src/main/java/com/example/pmdm_aplicacion/fragments/CamaraFragment.java
@@ -0,0 +1,29 @@
+package com.example.pmdm_aplicacion.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pmdm_aplicacion.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CamaraFragment extends Fragment {
+
+    public CamaraFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camara, container, false);
+    }
+}
